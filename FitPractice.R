42ae922aecba0864888321dab196b7df2463ffa8
rayanh <rtg5200@psu.edu>
2025-06-05 13:22:19 -0400

    adding likelihood fn and plot

--- FitPractice.R
+++ FitPractice.R
@@ -9,7 +9,7 @@ SIR_odes <- function (t,x,params){
   beta <- params[1] 
   gamma <- params[2] 
   dS <- -beta*S*I 
-  dI <- beta*S*I - gamma*I 
+  dI <- beta*S*I - gamma*I - alpha*I 
   dR <- gamma*I 
   list(c(dS,dI,dR))
   
@@ -17,21 +17,29 @@ SIR_odes <- function (t,x,params){
   }
 
 #parameters 
-gamma <- 1/100
-beta <- 1/1100
+
+#gamma = 0.005 
+gamma <- 0.005
+
+#beta = 0.0001 
+beta <- 0.0001  
+
+alpha <- 0.005 
+
+
 
 #times 
 
-t<- seq(0,1000,by= 1) 
+t<- seq(0,30000,by= 1) 
 
 results <- as.data.frame(ode(y = c(S=100, I=1, R=0), 
-                             times = t, func = SIR_odes, parms = c(beta,gamma))) 
+                             times = t, func = SIR_odes, parms = c(beta,gamma, alpha))) 
 
 plot(x =results$time, y=results$I, type = 'l', col = 'red', ylim = c(0,100), main = 'sir', xlab = 'time', ylab = 'number of infected people')
-lines(x = results$S , type = 'l', col= 'green')
+lines(x = results$S , type = 'l', col= 'black')
 lines(x = results$R,  type = 'l', col = 'blue') 
 legend("topright", title="key", legend=c("susceptible", "infected", "recovered"), 
-       col = c("green","red", "blue"),
+       col = c("black","red", "blue"),
         lty=c(1,1,1)) 
 
 
