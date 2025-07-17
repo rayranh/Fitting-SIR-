bcff9af0ba55aa1ecbd7440f3120802af3f9da64
rayanh <rtg5200@psu.edu>
2025-07-17 14:14:56 -0400

    attempting SIR version

--- SI_Gillespie.R
+++ SI_Gillespie.R
@@ -1,48 +1,57 @@
-library(ggplot2)
+library(ggplot2) 
+library(dplyr)
+rm(list = ls())
 
-si <- function(beta, N, S0, I0, tf) { 
-  time = 0 
-  S = S0 
-  I = I0 
+
+
+sir <- function(beta, gamma, N, S0, I0, R0, tf) { 
+  time <- 0 
+  S <- S0 
+  I <- I0 
+  R <- R0 
   
   #empty vectors 
-  ta = numeric(0)  
-  Sa = numeric(0) 
-  Ia = numeric(0) 
-  
+  ta <- numeric(0)  
+  Sa <- numeric(0) 
+  Ia <- numeric(0) 
+  Ra <- numeric(0)
   #while time is less than final time, update ta with new time 
-  while (time < tf) { 
-    ta = c(ta, time) 
-    Sa = c(Sa, S) 
-    Ia = c(Ia, I) 
+  while (time < tf) {  
+    ta <- c(ta, time)
+    Sa <- c(Sa, S)
+    Ia <- c(Ia, I)
+    Ra <- c(Ra, R)
     
-    w = beta * S * I
-    if (w == 0) break
+    t <- beta*S*I + gamma*I
+    if (t == 0) break
     
     #selecting random time, updating time 
-    dt = rexp(1, rate = w) 
+    dt <- rexp(1, t) 
     time = time + dt 
-    
     if (time > tf) break
     
-    ru = runif(1)
-    if (ru < 1) {  # there's only one event type: infection
-      S = S - 1
-      I = I + 1
-    }
+    w <- (beta*S*I)/ (beta*S*I + gamma*I)
+    randEvent = runif(1)
+    if (randEvent < w) { 
+      S<- S-1 
+      I<- I+1
+    } else {  
+      I<- I-1 
+      R<- R+1
+    } 
   }
   
-  return(data.frame(time = ta, S = Sa, I = Ia))
+  return(data.frame(time = ta, S = Sa, I = Ia, R= Ra))
 }  
 
-si_out = si(0.1/1000,1000,999,1,200)
-si_out_long = melt(si_out, "time") 
-ggplot(si_out_long, aes(x = time, y=value, colour = variable, group = variable)) + 
+sir_out = sir(beta = 0.1, gamma = 1/100, N= 1000, S0 = 999,I0 = 1, R0= 0,tf = 40000)
+sir_out_long = melt(sir_out, "time") 
+ggplot(sir_out_long, aes(x = time, y=value, colour = variable, group = variable)) + 
   geom_line(lwd=0.5) + 
   xlab("time") + ylab("Number")
 
 
-
+sir_out_long %>% filter(variable == "R")
 
 
 
