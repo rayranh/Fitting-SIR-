186440b9bd694ab383699eb4dee0d80c3d44bcb6
rayanh <rtg5200@psu.edu>
2025-07-28 09:55:16 -0400

    creating function and plot

--- SIRGillespie.R
+++ SIRGillespie.R
@@ -1,64 +1,71 @@
+rm(list = ls())
 library(ggplot2) 
 library(dplyr) 
 library(reshape2)
-rm(list = ls())
 
 time <- 0 
-timeEnd<- 100 
-
-
-beta = 0.001  
-gamma = 1/20  
-N = 101  
-
-S0 = 999 
-I0 = 1
-R0 =  0  
-
 
 S<- numeric(0)
 I <- numeric(0) 
 R <- numeric(0)   
 t <- numeric(0)
 
+set.seed(234)   
+GillespFunc <- function(beta, gamma, timeEnd, S0, I0, R0) {   
+  while (time < timeEnd) {  
+    
+    #need to update S and I 
+    S<-  c(S,S0) 
+    I <- c(I,I0) 
+    R <- c(R,R0)
+    t <- c(t,time)
+    
+    #updating time value with time till next event, the rate is the total rate for any event 
+    if (I0 == 0) break
+    dt <- rexp(1,rate = beta*S0*I0 + gamma*I0)    
+    time <- time + dt     
+    
+    
+    #creating random event 
+    
+    randEvent <- runif(1) 
+    
+    w <- (beta*S0*I0)/(beta*S0*I0+ gamma*I0) 
+    if (randEvent < w) { 
+      S0 = S0 -1 
+      I0 = I0 + 1 
+      R0 = R0 
+    } else {  
+      S0 = S0 
+      I0 = I0 - 1 
+      R0 = R0 + 1 
+    } 
+  }  
+  return(cbind(S,I,R,t))
+  }
 
 
-while (time < timeEnd) {  
-  
-  #need to update S and I 
-  S<-c(S, S0) 
-  I <-c(I,I0) 
-  R <- c(R,R0)
-  t <- c(t, time)
-  
-  #updating time value 
-  dt <- rexp(1,beta*S0*I0 + gamma*I0)
-  time <- time + dt    
-  
-  #creating random event 
-  randEvent <- runif(1) 
-  
-  w <- (beta*S0*I0)/(beta*S0*I0+ gamma*I0) 
-  if (randEvent < w) { 
-    S0 = S0 -1 
-    I0 = I0 + 1 
-    R0 = R0 
-  } else {  
-    S0 = S0 
-    I0 = I0 - 1 
-    R0 = R0 + 1 
-  } 
-} 
+# df <- as.data.frame(GillespFunc(beta = 0.001, gamma = 1/20, timeEnd = 200, S0 = 999, I0 = 1, R0 = 0)) 
 
-out_df <- data.frame (time = t, S=S, I=I, R=R) 
+out <- replicate(5, GillespFunc(beta = 0.001, gamma = 1/20, timeEnd = 100, S0 = 900, I0 = 100, R0 = 0), simplify = FALSE)
 
-df_long <- melt(out_df, id.vars = c("time"))
+
+# Convert each simulation to a data.frame and add an ID column
+sim_dfs <- lapply(seq_along(out), function(i) {
+  df <- as.data.frame(out[[i]])
+  colnames(df) <- c("S", "I", "R", "time")
+  df$sim <- paste0("Sim_", i)
+  return(df)
+})
+
+# Combine all into one data frame
+combined_df <- bind_rows(sim_dfs)
+
+# Reshape to long format for ggplot
+df_long <- melt(combined_df, id.vars = c("time", "sim"))
 
 # Plot
-ggplot(df_long, aes(x = time, y = value, color = variable)) + geom_line() + 
-  scale_color_manual (values = c("S"="black", "I" = "red", "R" = "blue"))+ 
-  labs(title = "Stochastic SIR Simulation", x = "Time", y = "Individuals") +
+ggplot(df_long, aes(x = time, y = value, color = variable)) +
+  geom_line() + scale_color_manual (values = c("S"="black", "I" = "red", "R" = "blue"))+ 
+  labs(title = "Gillespie SIR Simulations", x = "Time", y = "Count") +
   theme_minimal()
-
-
-
